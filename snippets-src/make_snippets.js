f9a458f9a96143ed2f3aec23c2583f7f15ee12e4
banxi1988 <banxi1988@gmail.com>
2018-05-14 13:24:38 +0800

    feat 完善 make_snippets.js 脚本

--- snippets-src/make_snippets.js
+++ snippets-src/make_snippets.js
@@ -1,26 +1,92 @@
 const fs = require('fs');
 const path = require('path');
 
-main('wepy_api.json', 'wepy_api', '.js');
-main('wepy_html.json', 'wepy_html', '.html');
-main('weui.json', 'weui', '.html');
-function main(name, folder, type) {
-  let data = fs.readFileSync(path.join(__dirname, name)).toString();
-  data = JSON.parse(data);
-
-  for (const key in data) {
-    if (data.hasOwnProperty(key)) {
-      const element = data[key];
-      if (!element.body) {
-        const _path = path.join(__dirname, 'includes', folder, key + type);
-        if (fs.existsSync(_path)) {
-          const _data = fs.readFileSync(_path).toString();
-          data[key].prefix = key;
-          data[key].body = _data;
-        }
+function includeSnippetBody(snippet, key, includesDir) {
+  // if we did not specify the body field
+  // then using the key as the filenameWithoutSuffix
+  // then find a matching file in `includes` dir.
+  const body = snippet["body"]
+  let filePath
+  if (body) {
+    if (typeof body !== 'string') {
+      return
+    }
+    if (!body.startsWith("@includes")) {
+      return
+    }
+    filePath = path.join(__dirname, body.substring(1))
+  } else {
+    // find the first matching file
+    for (const filename of fs.readdirSync(includesDir)) {
+      const lastDotIndex = filename.lastIndexOf('.')
+      const filenameWithoutSuffix = lastDotIndex > 0 ? filename.substring(0, lastDotIndex) : filename
+      const rcPath = path.join(includesDir, filename)
+      if (filenameWithoutSuffix === key && fs.statSync(rcPath).isFile()) {
+        filePath = rcPath
+        break
       }
+    }
+    if (!filePath) {
+      console.error("Failed to find default includes file:", { key: key, includesDir: includesDir })
+      return
     }
   }
 
-  fs.writeFileSync('./snippets/' + name, JSON.stringify(data));
+  if (!fs.existsSync(filePath)) {
+    console.error("Failed to find includes file: ", { body: body, key: key, filePath: filePath })
+    return
+  }
+
+  const bodyContent = fs.readFileSync(filePath, 'utf8')
+  const lines = []
+  const rawLines = bodyContent.match(/[^\r\n]+/g)
+  for (const line of rawLines) {
+    lines.push(line.trimRight())
+  }
+  snippet['body'] = lines
+
 }
+
+function ensureSnippetPrefix(snippet, key) {
+  if (!snippet.hasOwnProperty("prefix")) {
+    snippet.prefix = key
+  }
+}
+
+function mergeSnippetOptions(snippet, options) {
+  //  value in snippet should have high priority
+  const resultObj = options || {}
+  Object.assign(resultObj, snippet)
+  Object.assign(snippet, resultObj)
+}
+
+function makeSnippets(name, folder, global_options) {
+  const srcPath = path.join(__dirname, name);
+  if (!fs.existsSync(srcPath)) {
+    console.error("Failed to find source file:", srcPath);
+    return
+  }
+  const srcText = fs.readFileSync(srcPath, 'utf8');
+  const data = JSON.parse(srcText);
+  const includesDir = path.join(__dirname, "includes", folder)
+  for (const key in data) {
+    if (!data.hasOwnProperty(key)) {
+      continue;
+    }
+    const snippet = data[key];
+    ensureSnippetPrefix(snippet, key)
+    includeSnippetBody(snippet, key, includesDir)
+    if (global_options) {
+      mergeSnippetOptions(snippet, global_options)
+    }
+  }
+
+  const destFilePath = path.join(__dirname, '..', 'snippets', name)
+  console.info("Write snippets to ", destFilePath)
+  fs.writeFileSync(destFilePath, JSON.stringify(data, null, 2));
+}
+
+
+makeSnippets('wepy_api.json', 'wepy_api', { scope: "javascript,typescript" });
+makeSnippets('wepy_html.json', 'wepy_html', { scope: "vue, html, vue-html, wpy" });
+makeSnippets('weui.json', 'weui', { scope: "vue,html,vue-html,wpy" });
